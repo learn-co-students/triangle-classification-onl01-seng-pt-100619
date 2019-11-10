82f39a6f483ca8b7bac1be39b70c067feeffd392
allison cortez <allizetorc@gmail.com>
2019-11-10 21:50:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,27 +8,23 @@ attr_accessor :s1, :s2, :s3
   end
   
   def kind
-    
-    #we need triangle rules..
-    # The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. 
-    
-    # each side must be larger than 0.
-    
     if (s1*s2*s3) == 0 || (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s2 + s1) <= s3
       
       # raise
       begin
         raise TriangleError
-          puts error.message
+          # puts error.message
         end
       
-    elsif s1 == s2 && s1 == s3 
-      self.kind = :equilateral
+    elsif s1 == s2 && s1 == s3 && s2 == s3
+      :equilateral
     
     elsif s1 == s2 || s1 == s3 || s2 == s3
-      self.kind = :isosceles
+      :isosceles
       
-    #elsif
+    elsif s1 != s2 && s1 != s3 && s2 != s3
+      :scalene
+      
       
     end
   

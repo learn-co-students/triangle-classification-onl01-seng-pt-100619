5e11ee5c5a01ee626c6b2c40a5578d03516f99e4
Clyde Griffin <deezy13@gmail.com>
2019-12-04 04:52:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
   # write code here
+  
+  def initialize(equilateral, isosceles, scalene)
+    
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+    
+  end 
+  
+  def kind()
+    if @equilateral <= 0 || @isosceles <= 0 || @scalene <= 0 
+      raise TriangleError
+       elsif (@equilateral  + @isosceles <= @scalene) || (@equilateral + @scalene <= @isosceles) || (@isosceles+ @scalene <= @equilateral)
+      raise TriangleError
+    else
+      if (@equilateral == @isosceles) && (@isosceles == @scalene)
+        :equilateral
+      elsif (@equilateral == @isosceles) || (@isosceles == @scalene) || (@equilateral == @scalene)
+        :isosceles
+      elsif (@equilateral != @isosceles) && (@isosceles != @scalene) && (@equilateral != @scalene)
+        :scalene
+      end
+    end
+  end 
+  class TriangleError < StandardError
+    
+    
+    
+  end 
 end
